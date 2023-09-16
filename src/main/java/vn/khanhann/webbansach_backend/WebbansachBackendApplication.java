76c5e6f710244ba2877dc83afe9a2281d246e205
khahhan <khanhanbui1010@gmail.com>
2023-09-16 16:56:38 +0700

    Create web

---
+++ src/main/java/vn/khanhann/webbansach_backend/WebbansachBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.khanhann.webbansach_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbansachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbansachBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/khanhann/webbansach_backend/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.khanhann.webbansach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
